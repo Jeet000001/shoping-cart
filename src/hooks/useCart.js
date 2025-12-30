4023bd31dcf1eec80f49b3037160df1cd7a577c8
Jeet Paul <jeetpaul2222@gmail.com>
2025-12-30 21:20:44 +0530

    implement useCart hook for managing cart data in localStorage

---
+++ src/hooks/useCart.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+
+const useCart = () => {
+  // get cart data form localStorage
+  const [cart, setCart] = useState(() => {
+    try {
+      const savedCart = localStorage.getItem("cart");
+      return savedCart ? JSON.parse(savedCart) : [];
+    } catch (error) {
+      console.error("Faild to Load cart from localStorage", error);
+      return [];
+    }
+  });
+  // set cart data to localStorage
+  useEffect(() => {
+    try {
+      localStorage.setItem("cart", JSON.stringify(cart));
+    } catch (error) {
+      console.error("Faild to save cart to localStorage", error);
+    }
+  }, [cart]);
+
+  // Sync accross the tabs
+  useEffect(() => {
+    const handelStorage = (e) => {
+      if(e.ket === "cart"){
+        try {
+          const newCart = JSON.parse(e.newValue || "[]")
+          setCart(newCart)
+        } catch (error) {
+          console.error("Faild to parse cart from localStorage.", error)
+        }
+      }
+    }
+    window.addEventListener("Storage", handelStorage)
+    return () => window.removeEventListener("storage", handelStorage)
+  }, [])
+
+};
+
+export default useCart;
